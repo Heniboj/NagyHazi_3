f6655a664d9e9482918256a511de9ede41974f5f
heniboj <berenyihenrik2001@gmail.com>
2021-11-13 12:22:18 +0100

    járat felvétel hibakezelés fix

--- Programkód/src/VonatFelvetelDialog.java
+++ Programkód/src/VonatFelvetelDialog.java
@@ -4,6 +4,10 @@ public class VonatFelvetelDialog extends JaratFelvetelDialog {
 
     public class okButtonActionListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+            if(idField.getText().equals("") || ind_helyField.getText().equals("") || erk_helyField.getText().equals("")) {
+                new ErrorDialog("Ne hagyj üresen mezőt és ellenőrizd az adatok helyességét.");
+                return;
+            }
             try {
                 Datum ind = new Datum((String)ind_ev.getSelectedItem(), (String)(ind_honap.getSelectedItem()), (String)(ind_nap.getSelectedItem()), ind_ora.getText(), ind_perc.getText());
                 Datum erk = new Datum((String)(erk_ev.getSelectedItem()), (String)(erk_honap.getSelectedItem()), (String)(erk_nap.getSelectedItem()), erk_ora.getText(), erk_perc.getText());
